6ba6add56b0687b9967323b815807fde41b30d89
kmeljko <weljkodjuricic@gmail.com>
2023-10-25 10:30:55 +0200

    Absolute path to assets for app usage

--- lib/country_map_svg.dart
+++ lib/country_map_svg.dart
@@ -9,11 +9,12 @@ class CountryMapSvg {
     return SvgPicture.asset(
       "files/${code.toLowerCase()}-vector.svg",
       colorFilter: colorFilter,
+      package: "country_map_svg",
     );
   }
 
   static Future<String> getString(String code) async {
-    return await rootBundle
-        .loadString("files/${code.toLowerCase()}-vector.svg");
+    return await rootBundle.loadString(
+        "packages/country_map_svg/files/${code.toLowerCase()}-vector.svg");
   }
 }
--- test/country_map_svg_test.dart
+++ test/country_map_svg_test.dart
@@ -6,7 +6,7 @@ import 'package:country_map_svg/country_map_svg.dart';
 void main() {
   test('get country string ', () async {
     WidgetsFlutterBinding.ensureInitialized();
-    await CountryMapSvg.getString("bh");
+    print(await CountryMapSvg.getString("bh"));
   });
   testWidgets('get country widget ', (tester) async {
     WidgetsFlutterBinding.ensureInitialized();
